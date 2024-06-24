aa6980de3ccd6d6def33bca0b73fd1249d736998
Mateus Back <m.back@nextage.com.br>
2024-06-24 20:05:37 -0300

    aula 31

--- lib/app/view/contact_details.dart
+++ lib/app/view/contact_details.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:projeto_agenda/app/domain/entities/contact.dart';
 import 'package:projeto_agenda/app/view/contact_details_back.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ContactDetails extends StatelessWidget {
+  showModalError(BuildContext context) {
+    var alert = AlertDialog(
+      title: Text('Alerta!'),
+      content: Text('Não foi possível encontrar uma aplicação compatível.'),
+    );
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return alert;
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     var _back = ContactDetailsBack(context);
@@ -16,6 +28,7 @@ class ContactDetails extends StatelessWidget {
 
         return Scaffold(
           body: ListView(
+            padding: const EdgeInsets.all(40),
             children: [
               (Uri.tryParse(contact.urlAvatar!)!.isAbsolute)
                   ? CircleAvatar(
@@ -37,18 +50,42 @@ class ContactDetails extends StatelessWidget {
                 child: ListTile(
                   title: Text('Telefone'),
                   subtitle: Text('${contact.telefone}'),
+                  trailing: Container(
+                    width: width / 4,
+                    child: Row(
+                      children: [
+                        IconButton(
+                          onPressed: () {
+                            _back.launchPhone(showModalError);
+                          },
+                          icon: const Icon(Icons.phone),
+                          color: Colors.blue,
+                        ),
+                        IconButton(
+                          onPressed: () {
+                            _back.launchSms(showModalError);
+                          },
+                          icon: const Icon(Icons.message),
+                          color: Colors.blue,
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
               ),
               Card(
                 child: ListTile(
                   title: Text('E-mail'),
+                  onTap: () {
+                    _back.launchEmail(showModalError);
+                  },
                   subtitle: Text('${contact.email}'),
                 ),
               ),
             ],
           ),
           floatingActionButton: FloatingActionButton(
-            child: const Icon(Icons.arrow_back),
+            child: const Icon(Icons.arrow_back, color: Colors.white),
             onPressed: () {
               _back.goToBack();
             },
--- lib/app/view/contact_details_back.dart
+++ lib/app/view/contact_details_back.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:projeto_agenda/app/domain/entities/contact.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ContactDetailsBack {
   BuildContext context;
@@ -11,5 +12,45 @@ class ContactDetailsBack {
 
   goToBack() {
     Navigator.of(context).pop();
+  }
+
+  Future<void> _launchApp(
+      String? url,
+      Function(BuildContext context) showModalError,
+      BuildContext context) async {
+    if (url == null || url.isEmpty) {
+      showModalError(context);
+      return;
+    }
+
+    final Uri? urlParsed = Uri.tryParse(url);
+
+    if (urlParsed == null) {
+      showModalError(context);
+      return;
+    }
+
+    try {
+      final bool canLaunch = await canLaunchUrl(urlParsed);
+      if (canLaunch) {
+        await launchUrl(urlParsed);
+      } else {
+        showModalError(context);
+      }
+    } catch (e) {
+      showModalError(context);
+    }
+  }
+
+  launchPhone(Function(BuildContext context) showModalError) {
+    _launchApp('tel:${contact?.telefone}', showModalError, context);
+  }
+
+  launchSms(Function(BuildContext context) showModalError) {
+    _launchApp('sms:${contact?.telefone}', showModalError, context);
+  }
+
+  launchEmail(Function(BuildContext context) showModalError) {
+    _launchApp('mailto:${contact?.email}', showModalError, context);
   }
 }
