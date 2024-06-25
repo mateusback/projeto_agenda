f8678dec3f326ea0f9f35899289ea49251cd0a0e
Mateus Back <m.back@nextage.com.br>
2024-06-24 21:06:33 -0300

    aula 34

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:projeto_agenda/app/injection.dart';
 import 'app/my_app.dart';
@@ -5,4 +7,11 @@ import 'app/my_app.dart';
 void main() async {
   setupInjection();
   runApp(MyApp());
+  await Firebase.initializeApp();
+
+  FirebaseFirestore.instance.collection('contact').doc().set({
+    'name': 'Joaquim',
+    'phone': '(11) 9 98529632',
+    'email': 'joaqui@email.com',
+  });
 }
