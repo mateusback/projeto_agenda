9aa5da72a8d28283f7076d1dcbc45628d6728cde
Mateus Back <m.back@nextage.com.br>
2024-06-25 19:46:26 -0300

    aula 35

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,15 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:projeto_agenda/app/injection.dart';
+import 'package:projeto_agenda/firebase_options.dart';
 import 'app/my_app.dart';
 
 void main() async {
   setupInjection();
   runApp(MyApp());
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   FirebaseFirestore.instance.collection('contact').doc().set({
     'name': 'Joaquim',
